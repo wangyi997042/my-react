88303d50a9627fb1d513470f7f191f497efcf4f4
wangyi <wangyi997042@163.com>
2023-05-15 15:22:24 +0800

    rc-feild-form

--- src/pages/base/components/my-rc-field-form/Form.js
+++ src/pages/base/components/my-rc-field-form/Form.js
@@ -1,26 +1,27 @@
 import React from "react";
-import FieldContext from './FieldContext';
-import useForm from './useForm'
+import useForm from "./useForm";
+import FieldContext from "./FieldContext";
 
 export default function Form({form, children, onFinish, onFinishFailed}, ref) {
-    const [formInstance] = useForm(form);
+  const [formInstance] = useForm(form);
 
+  // 子-》父
+  React.useImperativeHandle(ref, () => formInstance);
 
-    React.useImperativeHandle(ref, () => formInstance);
-    console.log('formInstance====', formInstance);
+  formInstance.setCallbacks({
+    onFinish,
+    onFinishFailed,
+  });
 
-    formInstance.setCallback({
-        onFinish,
-        onFinishFailed
-    })
-    return (
-        <form
-            onSubmit={e => {
-                e.preventDefault();
-                formInstance.submit();
-            }}
-        >
-            <FieldContext.Provider>{children}</FieldContext.Provider>
-        </form>
-    )
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        formInstance.submit();
+      }}>
+      <FieldContext.Provider value={formInstance}>
+        {children}
+      </FieldContext.Provider>
+    </form>
+  );
 }
